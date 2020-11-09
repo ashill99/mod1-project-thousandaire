9a58d190d3c4948bfee5b73ee963f22040d2b674
ashill99 <admshng@gmail.com>
2020-11-09 15:44:26 -0500

    added question

--- db/migrate/20201109204029_AddTableToQuestion.rb
+++ db/migrate/20201109204029_AddTableToQuestion.rb
@@ -3,7 +3,7 @@ class AddTableToQuestion < ActiveRecord::Migration[5.2]
     create_table :questions do |t|
       t.integer :value_of_question
       t.string :difficulty
-      t.question :difficulty
+      t.string :question
       t.string :correct_answer 
       t.string :incorrect_answer_1
       t.string :incorrect_answer_2
