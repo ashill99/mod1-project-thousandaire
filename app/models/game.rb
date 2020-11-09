e4847f553904cce3e2f260e5848474e90758e643
ashill99 <admshng@gmail.com>
2020-11-09 17:28:03 -0500

    updated relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
 
     belongs_to :user 
     has_many :game_questions 
-    has many :questions, through: :game_questions
+    has_many :questions, through: :game_questions
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,10 @@
 require_relative '../config/environment'
 
+
+def greet 
+    puts "Welcome to Who Wants to be a Thousandire?"
+end
+
+greet 
+
 # puts "hello world"
